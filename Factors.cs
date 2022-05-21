7c30105c24e53dcb86f83f6315a15bf5c76136a8
milanbiranwar <102604173+milanbiranwar@users.noreply.github.com>
2022-05-22 05:03:59 +0530

    Update Factors.cs

--- Factors.cs
+++ Factors.cs
@@ -10,30 +10,19 @@ namespace Practice
     {
                public static void factor()
                {
-			int n;
-			Console.Write("Enter Your Number : ");
-			n = Convert.ToInt32(Console.ReadLine());
-			// Printing the prime factors
-			Console.Write("The Prime Factors of " + n + " are : ");
-
-
-			int i = 2;
-			while (n > 1)
-			{
-				if (n % i == 0)
-				{
-					Console.Write(i + " ");
-					n = n / i;
-				}
-				else
-				{
-					i++;
-				}
-			}
-			
-			Console.WriteLine(" ");
-			Console.ReadLine();
-	       }
+			 int n, i;
+                    Console.WriteLine("Enter the Number:");
+                    n = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Factors of NUmber are:");
+                       for (i = 1; i <= n; i++)
+                       {
+                          if (n % i == 0)
+                           {
+                             Console.WriteLine(i);
+                            }
+                        }
+	          }
+	       
       }
 		      
 }
