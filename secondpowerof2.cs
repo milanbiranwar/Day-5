13ec5807db9d83766d80857110e67ead0fc91ead
milanbiranwar <102604173+milanbiranwar@users.noreply.github.com>
2022-05-22 07:29:56 +0530

    Update secondpowerof2.cs

--- secondpowerof2.cs
+++ secondpowerof2.cs
@@ -10,34 +10,16 @@ namespace Practice
     {
         public static void secondpower()
         {
-            try
+           
+            int m = 2;
+            int n = 31;
+            int val = 1;
+
+            for (int i = 0; i < n; i++)
             {
-                int no;
-                int remainder;
-                Console.WriteLine("Enter a no to check");
-                no = Convert.ToInt32(Console.ReadLine());
-                if (no == 1)
-                    Console.Write("It is power of 2.");
-                else
-                {
-                    while (no > 1)
-                    {
-                        remainder = no % 2;
-                        if (remainder != 0)
-                            break;
-                        no /= 2;
-                    }
-                    if (no == 1)
-                        Console.WriteLine("It is a power of 2");
-                    else
-                        Console.WriteLine("It is not a power of 2");
-                }
+                val = val*m;
+                Console.WriteLine(val);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.ReadKey();
         }
     }
 }
