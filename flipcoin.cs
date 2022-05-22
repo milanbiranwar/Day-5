0aa8d9815d07406ef829fed3884b178ee3e6b8ef
milanbiranwar <102604173+milanbiranwar@users.noreply.github.com>
2022-05-22 06:25:32 +0530

    Update flipcoin.cs

--- flipcoin.cs
+++ flipcoin.cs
@@ -9,30 +9,31 @@ namespace Practice
     internal class flipcoin
 
 	{
-		static Random rnd = new Random();
-		public static void flipcoinprogram()
-		{
-			Console.WriteLine("Welcome to the coin flip Game");
-
-			Random sc = new Random();
-
-			Console.WriteLine("Enter Your Number: ");
-			
-			int headCount = 0, tailCount = 0;
-			int count = sc.Next();
-
-			for (int i = 0; i < count; i++)
-			{
-				double random = rnd.NextDouble();
-
-				if (random < 0.5)
-					tailCount++;
-				else
-					headCount++;
-			}
-			Console.WriteLine("Pecentage of tails(" + tailCount + "): " + (tailCount * 100f / (headCount + tailCount)));
-			Console.WriteLine("Percentage of Head(" + headCount + "): " + (headCount * 100f / (headCount + tailCount)));
-
+		 Random Rand = new Random();
+            int heads = 0;
+            
+            int tails = 0;
+            
+            int result = 0;
+       
+            for (int i = 0; i < 10000; i++)
+                
+            {
+                result = Rand.Next(0, 2);
+                if (result == 1)
+                    
+                {
+                    heads++;   
+                }
+                else
+                {
+                    tails++;
+                }
+   
+            }
+            Console.WriteLine("heads was flipped {0} times", heads);
+            Console.WriteLine("tails was flipped {0} times", tails);
+            return result;
 		}
 
 	}
