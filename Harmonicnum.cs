b712f043866e79ded2cea362207176d4569d5c3a
milanbiranwar <102604173+milanbiranwar@users.noreply.github.com>
2022-05-22 05:44:40 +0530

    Update Harmonicnum.cs

--- Harmonicnum.cs
+++ Harmonicnum.cs
@@ -8,25 +8,21 @@ namespace Practice
 {
     internal class Harmonicnum
     {
-        public static void harmonicnumber()
+         public static void harmonicnumber()
         {
-			// Assigning the variables
-			double num = 0, result = 0, i;
-
-			Random sc = new Random();
-
-			//Taking data from users (Assuming Any number is a Harmonic_number)
+			double num = 0;
+				int  i;
 			Console.Write("Enter the Number: ");
-			int n = sc.Next();
+			int n = Convert.ToInt32(Console.ReadLine());
 
-			//Condition by using for loop
 			for (i = 1; i <= n; i++)
-			{
-				num = num + (1 / i);
-				result = num;
+			{ 
+				Console.WriteLine("1/{0}",+i);
+				num = num + (1 /(float)i);
+				
 			}
-			Console.WriteLine("The value of harmonic no. " + n + " is: " + result);
-
+			Console.Write("Sum of Series upto{0} terms : {1} ", n, num);
+			Console.ReadLine();
 		}
 
 	}
